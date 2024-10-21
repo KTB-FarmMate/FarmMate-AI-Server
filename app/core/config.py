c3c3f174e59f34b6e23fb04834bf9a6765c6676f
joeun <whdms1107@gmail.com>
2024-10-21 14:43:35 +0900

    📝 docs: .env 파일의 KAKAO_LOCAL_API_KEY 추가
    새로운 기능에 필요한 환경변수 선언에 필요한 key를 추가

--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,7 @@ load_dotenv()
 class Settings(BaseSettings):
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
     ASSISTANT_ID: str = os.getenv("ASSISTANT_ID")
+    KAKAO_LOCAL_API_KEY: str = os.getenv("KAKAO_LOCAL_API_KEY")
 
 
 settings = Settings()
