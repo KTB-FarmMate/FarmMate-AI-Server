d55a5bba61d47f9ab9b03b05c6fa14349bfa12eb
joeun <whdms1107@gmail.com>
2024-12-17 03:22:17 +0900

    🐛 fix: dashboard 구성

--- app/front/static/js/dashboard.js
+++ app/front/static/js/dashboard.js
@@ -2,15 +2,35 @@ document.addEventListener("DOMContentLoaded", () => {
     // 현재 날씨 정보 요청
     let select_crop = localStorage.getItem("select_crop");
     let crop_data = JSON.parse(localStorage.getItem("crops_data"))[select_crop];
+    let memberId = localStorage.getItem("memberId");
+    let temperature = document.querySelector(".temperature");
+    let humidity = document.querySelector(".humidity");
+
     fetch(`${BE_SERVER}/weather/current?address=${crop_data.address}`)
         .then(response => {
-            if (response.ok){
+            if (response.ok) {
                 return response.json();
             }
         }).then(data => {
-            console.log(data);
+        temperature.textContent = data.temperature;
+        humidity.textContent = data.humidity;
+        console.log(data);
     })
-
     // 병해충 알림 요청
     // 재배 길라잡이 요청
+    fetch(`${BE_SERVER}/members/${memberId}/threads/${crop_data.threadId}/status`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(data => {
+            console.log(data);
+            const card_ul = document.querySelector(".card_content ul");
+            data["recommendedActions"].forEach((action) => {
+                let li = document.createElement("li");
+                li.textContent = action
+                card_ul.appendChild(li);
+            })
+        })
 })
