3825de54b839669ed9bea1ea805faf77d29e0588
joeun <whdms1107@gmail.com>
2024-12-17 05:10:49 +0900

    🐛 fix: 대시보드 요청 등록

--- app/front/static/js/dashboard.js
+++ app/front/static/js/dashboard.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+
+})
+
+
+function get_weather(memberId, cropName) {
     // 현재 날씨 정보 요청
-    const select_crop = localStorage.getItem("select_crop");
-    if (!select_crop) {
-        const select_crop = decodeURIComponent(window.location.pathname.split("/")[3]);
-        localStorage.setItem("select_crop", select_crop);
-    }
-    const crop_data = JSON.parse(localStorage.getItem("crops_data"))[select_crop];
-    const memberId = localStorage.getItem("memberId");
+    const crop_data = JSON.parse(localStorage.getItem("crops_data"))[cropName];
     const temperature = document.querySelector(".temperature");
     const humidity = document.querySelector(".humidity");
 
@@ -20,9 +20,48 @@ document.addEventListener("DOMContentLoaded", () => {
         humidity.textContent = data.humidity;
         console.log(data);
     })
-    // 병해충 알림 요청
-    // 재배 길라잡이 요청
-    fetch(`${BE_SERVER}/members/${memberId}/threads/${crop_data.threadId}/status`)
+}
+
+// 병해충 알림 요청
+function get_pests(cropName) {
+    // 현재 날씨 정보 요청
+    fetch(`${BE_SERVER}/pests?cropName=${cropName}`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(data => {
+            const warnings = document.querySelector(".alert-warnings");
+            warnings.innerHTML = '';
+            data["warnings"].forEach(element => {
+                const span = document.createElement("span");
+                span.textContent = element;
+                warnings.appendChild(span);
+            })
+            const advisories = document.querySelector(".alert-advisories");
+            advisories.innerHTML = '';
+            data["advisories"].forEach(element => {
+                const span = document.createElement("span");
+                span.textContent = element;
+                advisories.appendChild(span);
+            })
+            const foreCasts = document.querySelector(".alert-foreCasts");
+            foreCasts.innerHTML = '';
+            data["foreCasts"].forEach(element => {
+                const span = document.createElement("span");
+                span.textContent = element;
+                foreCasts.appendChild(span);
+            })
+            console.log(data);
+        })
+}
+
+// 재배 길라잡이 요청
+function get_guidance(memberId, cropName) {
+    const crops_data = JSON.parse(localStorage.getItem("crops_data"));
+    const select_crop = crops_data[cropName];
+    fetch(`${BE_SERVER}/members/${memberId}/threads/${select_crop.threadId}/status?cropId=${select_crop.cropId}`)
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -37,4 +76,4 @@ document.addEventListener("DOMContentLoaded", () => {
                 card_ul.appendChild(li);
             })
         })
-})
+}
