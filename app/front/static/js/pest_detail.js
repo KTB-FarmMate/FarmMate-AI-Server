cde40fc314a04732a0be76675433aaf77dacb905
joeun <whdms1107@gmail.com>
2024-12-18 14:11:12 +0900

    🐛 fix: query selector 오탈자 수정

--- app/front/static/js/pest_detail.js
+++ app/front/static/js/pest_detail.js
@@ -1,24 +1,24 @@
 function get_pest_detail(cropName, pestName) {
     // 작물명 한/중/영
-    const name_kor = document.querySelector(".fest_name .kor");
-    const name_chn = document.querySelector(".fest_name .chn");
-    const name_eng = document.querySelector(".fest_name .eng");
+    const name_kor = document.querySelector(".fest_name.kor");
+    const name_chn = document.querySelector(".fest_name.chn");
+    const name_eng = document.querySelector(".fest_name.eng");
     const crop_name = document.querySelector(".crop_name");
 
     // 질병 이미지 정보
-    const fest_img_list = document.querySelector(".fests_img_container .img_list");
+    const fest_img_list = document.querySelector(".fests_img_container.img_list");
     // 질병 표지 정보
-    const fest_preview_img = document.querySelector(".fest_img .img");
+    const fest_preview_img = document.querySelector(".fest_img.img");
 
 
     // 발생 환경
-    const occurrence_env_content = document.querySelector(".occurrence_env_container .content");
+    const occurrence_env_content = document.querySelector(".occurrence_env_container.content");
 
     // 증상 설명
-    const description_content = document.querySelector(".occurrence_env_container .content");
+    const description_content = document.querySelector(".occurrence_env_container.content");
 
     // 예방 방법
-    const control_content = document.querySelector(".control_container .content");
+    const control_content = document.querySelector(".control_container.content");
     fetch(`${BE_SERVER}/pests/${pestName}?cropName=${cropName}`)
         .then(response => {
             if (response.ok) {
@@ -27,19 +27,19 @@ function get_pest_detail(cropName, pestName) {
             throw new Error(response.statusText);
         })
         .then(data => {
-            name_kor.innerHTML = data?.["sickNameKor"] ?? "";
-            name_chn.innerHTML = data?.["sickNameChn"] ?? "";
-            name_eng.innerHTML = data?.["sickNameEng"] ?? "";
-            crop_name.innerHTML = data?.["cropName"] ?? "";
+            name_kor.textContent = `한글명: ${data?.sickNameKor || ""}`;
+            name_chn.textContent = `한문명: ${data?.sickNameChn || ""}`;
+            name_eng.textContent = `영문명: ${data?.sickNameEng || ""}`;
+            crop_name.textContent = `작물명: ${data?.cropName || ""}`;
 
             // 발생 환경
-            occurrence_env_content.innerHTML = data?.["developmentCondition"] ?? "";
+            occurrence_env_content.textContent = data?.["developmentCondition"] ?? "";
 
             // 증상
-            description_content.innerHTML = data?.["symptoms"] ?? "";
+            description_content.textContent = data?.["symptoms"] ?? "";
 
             // 예방법
-            control_content.innerHTML = data?.["preventionMethod"] ?? "";
+            control_content.textContent = data?.["preventionMethod"] ?? "";
 
             // 이미지 리스트
             const image_list = data?.["imageList"] ?? [];
